b2b1e7a6285f7b79ac5d385a9b6b6b9a68d05cef
akhil-html <65633373+akhil-html@users.noreply.github.com>
2021-06-23 23:29:29 -0400

    Initial commit

---
+++ IsItALeapYear/IsItALeapYear/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace IsItALeapYear
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public bool IsItALeapYear(int year)
+        {
+            if (year % 400 == 0)
+            {
+                return true;
+            }else if(year%100 == 0)
+            {
+                return false;
+            }else if (year % 4 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
